ad88d5f987ffb7a819fd4c22b55bb3483e8dd79b
Elizabeth Chapman <elizabethmaychapman@gmail.com>
2023-10-12 18:31:11 +0100

    progress save

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "aside main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        Nav
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Nav
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
