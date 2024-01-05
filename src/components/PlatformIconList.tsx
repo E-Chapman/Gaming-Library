fdf1270a77bee20e66f2fcea2cff251141e00784
Elizabeth Chapman <elizabethmaychapman@gmail.com>
2024-01-05 22:06:43 +0000

    Refactor: remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,7 +10,7 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import { IconType } from "react-icons";
 
 interface Props {
@@ -28,6 +28,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     mac: FaApple,
     nintendo: SiNintendo,
     web: BsGlobe,
+    iphone: MdPhoneIphone,
   };
 
   return (
